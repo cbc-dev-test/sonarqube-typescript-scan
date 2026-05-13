ac7c83543f4c87128780525286a05be3c21cba6d
SindhaPerumal <sperumal@cloudbees.com>
2026-05-13 09:41:13 +0530

    Removed noent

--- src/xxe.ts
+++ src/xxe.ts
@@ -4,6 +4,5 @@ const libxmljs = require('libxmljs');
 const xml = fs.readFileSync('xxe.xml', 'utf8');
 libxmljs.parseXmlString(xml, {
   noblanks: true,
-  noent: true, // Noncompliant: expands entities, can lead to XXE
   nocdata: true,
 });
